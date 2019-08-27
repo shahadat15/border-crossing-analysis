2bc1d5e064e8b0063d787b448bcdb5fa94bb2df3
shahadat Iqbal <miqba005@fiu.edu>
2019-08-26 22:01:37 -0400

    Update border_analytics.py

--- src/border_analytics.py
+++ src/border_analytics.py
@@ -13,47 +13,59 @@ output_file_location = sys.argv[2]
 #  (Value) of each type of vehicle or equipment, or passengers or pedestrians, that crossed the border that month, 
 #  regardless of what port was used.
 
-def read_lines(input_link):
+def read_file_store_into_dictionary(input_file_location):
     # Define an empty dictionary
     input_data_dictionary = {}
     
-    with open(input_link) as myfile:
-        # skip the first line which is a header
-        line = next(myfile)
-        # Read rest of the lines
-        for line in myfile:
-            # split the comma separated words
-            words = line.strip().split(',')
-            # Check wheather the dictionary key is already exists or not. If not exists then declare it. 
-            # Otherwise, sum up the current value to the dictionary value
-            if words[3] in input_data_dictionary.keys():
-                if words[5] in input_data_dictionary[words[3]].keys():
-                    if words[4] in input_data_dictionary[words[3]][words[5]].keys():
-                        input_data_dictionary[words[3]][words[5]][words[4]] +=int(words[6])
+    try:
+        with open(input_file_location) as _file:
+            # Read the first line which is a header
+            _line = next(_file)
+            header_input_file = _line.strip().split(',')
+            header_actual = ['Port Name','State','Port Code','Border','Date','Measure','Value','Location']
+ 
+            if header_input_file != header_actual:
+                return(print("ERROR: Check the input file: \nPossible causes: \n1. Either all variables are not present \n2. Variables are not in right order \n3. Header is not present"))   
+            
+            # Read rest of the lines
+            for _line in _file:
+                # split the comma separated words
+                words = _line.strip().split(',')
+                try: int(words[6]) ## Check if any NA values are found in the "Value" field
+                except: return (print("Error: Unacceptable (or NA) values are Found"))
+  
+                   
+                # Check wheather the dictionary key is already exists or not. If not exists then declare it. 
+                # Otherwise, sum up the current value to the dictionary value
+                if words[3] in input_data_dictionary.keys():
+                    if words[5] in input_data_dictionary[words[3]].keys():
+                        if words[4] in input_data_dictionary[words[3]][words[5]].keys():
+                            input_data_dictionary[words[3]][words[5]][words[4]] +=int(words[6])
+                        else:
+                            input_data_dictionary[words[3]][words[5]][words[4]] = int(words[6])
                     else:
-                        input_data_dictionary[words[3]][words[5]][words[4]] = int(words[6])
+                        input_data_dictionary[words[3]][words[5]] ={}
+                        if words[4] in input_data_dictionary[words[3]][words[5]].keys():
+                            input_data_dictionary[words[3]][words[5]][words[4]] +=int(words[6])
+                        else:
+                            input_data_dictionary[words[3]][words[5]][words[4]] = int(words[6])
                 else:
-                    input_data_dictionary[words[3]][words[5]] ={}
-                    if words[4] in input_data_dictionary[words[3]][words[5]].keys():
-                        input_data_dictionary[words[3]][words[5]][words[4]] +=int(words[6])
+                    input_data_dictionary[words[3]] = {}
+                    if words[5] in input_data_dictionary[words[3]].keys():
+                        if words[4] in input_data_dictionary[words[3]][words[5]].keys():
+                            input_data_dictionary[words[3]][words[5]][words[4]] +=int(words[6])
+                        else:
+                            input_data_dictionary[words[3]][words[5]][words[4]] = int(words[6]) 
                     else:
-                        input_data_dictionary[words[3]][words[5]][words[4]] = int(words[6])
-            else:
-                input_data_dictionary[words[3]] = {}
-                if words[5] in input_data_dictionary[words[3]].keys():
-                    if words[4] in input_data_dictionary[words[3]][words[5]].keys():
-                        input_data_dictionary[words[3]][words[5]][words[4]] +=int(words[6])
-                    else:
-                        input_data_dictionary[words[3]][words[5]][words[4]] = int(words[6]) 
-                else:
-                    input_data_dictionary[words[3]][words[5]] ={}
-                    if words[4] in input_data_dictionary[words[3]][words[5]].keys():
-                        input_data_dictionary[words[3]][words[5]][words[4]] +=int(words[6])
-                    else:
-                        input_data_dictionary[words[3]][words[5]][words[4]] = int(words[6])
-    myfile.close() # Close the open file link
-    return input_data_dictionary
-
+                        input_data_dictionary[words[3]][words[5]] ={}
+                        if words[4] in input_data_dictionary[words[3]][words[5]].keys():
+                            input_data_dictionary[words[3]][words[5]][words[4]] +=int(words[6])
+                        else:
+                            input_data_dictionary[words[3]][words[5]][words[4]] = int(words[6])
+        _file.close() # Close the open file link
+        return input_data_dictionary
+    except:
+        return (print('ERROR: Check input file or file location'))
 
 
 # In order to sort the data, the dictionary data is saved as an array with each line as a data_point class.
@@ -73,7 +85,7 @@ class data_point:
     
 
 # The final function is used to get the moving average of the value and saved the data into an multidimentional array
-def data_process(input_data_dictionary):
+def data_process_get_average(input_data_dictionary):
     # define a new array 
     processed_data = []
 
@@ -83,47 +95,50 @@ def data_process(input_data_dictionary):
             # For a spefic border type and measure, the value is stored to another dictionary. This dictionary has key of year. 
             # For each year it stores the twelve months crossing value
             value_months = {}
-            for date_key in input_data_dictionary[border_key][measure_key]:
-                yr = date_key.split(" ")[0].split("/")[2]
-                mn = date_key.split(" ")[0].split("/")[0]
-                if yr in value_months.keys():
-                    value_months[yr][int(mn)-1]= int(input_data_dictionary[border_key][measure_key][date_key])
-                else:
-                    # If key is not present then declare the value with a list of 12 values.
-                    # Each value of the list represnts the month in chronological order
-                    value_months[yr] = [0,0,0,0,0,0,0,0,0,0,0,0]
-                    value_months[yr][int(mn)-1]= int(input_data_dictionary[border_key][measure_key][date_key])
+            for date_key1 in input_data_dictionary[border_key][measure_key]:
+                try:
+                    yr = date_key1.split(" ")[0].split("/")[2]
+                    mn = date_key1.split(" ")[0].split("/")[0]
+                    if yr in value_months.keys():
+                        value_months[yr][int(mn)-1]= int(input_data_dictionary[border_key][measure_key][date_key1])
+                    else:
+                        # If key is not present then declare the value with a list of 12 values.
+                        # Each value of the list represnts the month in chronological order
+                        value_months[yr] = [0,0,0,0,0,0,0,0,0,0,0,0]
+                        value_months[yr][int(mn)-1]= int(input_data_dictionary[border_key][measure_key][date_key1])
+                except:
+                    return (print("Error: Date is not in right format"))
+                
 
             # Now based on value_months dictionary data the moving average is calculated and saved it into processed_data array
             # the second value of the input_data_dictionary value list represents the moving average
-            for date_key in input_data_dictionary[border_key][measure_key]:
-                yr = date_key.split(" ")[0].split("/")[2]
-                mn = int(date_key.split(" ")[0].split("/")[0])
+            for date_key2 in input_data_dictionary[border_key][measure_key]:
+                yr = date_key2.split(" ")[0].split("/")[2]
+                mn = int(date_key2.split(" ")[0].split("/")[0])
                 if mn == 1:
-                    processed_data.append(data_point(border_key,date_key,measure_key,
-                                                  (input_data_dictionary[border_key][measure_key][date_key]),
+                    processed_data.append(data_point(border_key,date_key2,measure_key,
+                                                  (input_data_dictionary[border_key][measure_key][date_key2]),
                                                   0,yr,mn))
                 elif (mn == 2):
-                    processed_data.append(data_point(border_key,date_key,measure_key,
-                                                  (input_data_dictionary[border_key][measure_key][date_key]),
+                    processed_data.append(data_point(border_key,date_key2,measure_key, 
+                                                  (input_data_dictionary[border_key][measure_key][date_key2]),
                                                   (value_months[yr][0]),yr,mn))
                 else:
                     a = sum(value_months[yr][0:int(mn)-1])
                     b = (int(mn)-1)
-                    processed_data.append(data_point(border_key,date_key,measure_key,
-                                                  (input_data_dictionary[border_key][measure_key][date_key]),
+                    processed_data.append(data_point(border_key, date_key2, measure_key,
+                                                  (input_data_dictionary[border_key][measure_key][date_key2]),
                                                   (-(-a//b)),yr,mn))
     return (processed_data)
-	
 
 ###  Read the input data file and insert the data into a dictionary
 ## define the input file. Default locaiton with file name is './input/Border_Crossing_Entry_Data.csv'
 # Read the data and get them as a Dictionary
-input_data_dictionary = read_lines(input_file_location)
+input_data_dictionary = read_file_store_into_dictionary(input_file_location)
 
 
 ## Process the data, get moving average, and insert the data into a multidimentional array
-processed_data = data_process(input_data_dictionary)
+processed_data = data_process_get_average(input_data_dictionary)
 
 
 ## The final step is to sort the data
@@ -134,8 +149,11 @@ processed_data_ordered = sorted(processed_data, key=lambda data_point:
 						
 
 
-f1= open(output_file_location,"w+")
-f1.write("Border,Date,Measure,Value,Average\n")
-for row in processed_data_ordered:
-    f1.write(str(row.border)+","+str(row.date)+","+str(row.measure)+","+str(row.value)+","+str(row.average)+"\n")
-f1.close()
+try:
+    output_file= open(output_file_location,"w+")
+    output_file.write("Border,Date,Measure,Value,Average\n")
+    for row in processed_data_ordered:
+        output_file.write(str(row.border)+","+str(row.date)+","+str(row.measure)+","+str(row.value)+","+str(row.average)+"\n")
+    output_file.close()
+except:
+    print ("ERROR: Check output file or file location")
